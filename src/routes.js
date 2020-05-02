330974909d45c7a8622b4e83c7e045f161639aaa
stefanus dody <stefanusdody@yahoo.com>
2020-05-02 08:06:49 +0700

    new updates

--- src/routes.js
+++ src/routes.js
@@ -1,78 +1,97 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import AdminRoute from "./auth/adminroute";
-import PrivateRoute from "./auth/privateroute";
+import AdminDashboard from "./user/admindashboard";
 import AddCategory from "./admin/addcategory";
 import AddProduct from "./admin/addproduct";
+import Cart from "./core/cart";
+import CheckOut from "./core/checkout";
+import Dashboard from "./user/dashboard";
+import Home from "./core/home";
 import ManageProducts from "./admin/manageproducts";
 import ManagePayments from "./admin/managepayments";
 import Orders from "./admin/orders";
-import UpdateProduct from "./admin/updateproduct";
+import PaymentConfirmation from "./core/paymentconfirmation";
+import PrivateRoute from "./auth/privateroute";
 import Profile from "./user/profile";
 import PurchaseHistory from "./user/purchasehistory";
-import SignIn from "./user/signin";
-import SignUp from "./user/signup";
-import Dashboard from "./user/dashboard";
-import AdminDashboard from "./user/admindashboard";
-import Cart from "./core/cart";
-import CheckOut from "./core/checkout";
-import Home from "./core/home";
-import PaymentConfirmation from "./core/paymentconfirmation";
+import PrimarySearchAppBar from "./core/Appbar"
 import Review from "./core/review";
-import Shop from "./core/shop";
-import NavigationBar from "./core/navbar";
+import SignUp from "./user/signup";
+import SignIn from "./user/signin";
 import Product from './core/product';
+import Shop from "./core/shop";
+import Order from "./core/order";
+import ContactUs from "./core/contact";
+import ResiShipment from "./core/resishipment";
+import ReviewProducts from "./core/reviewproducts";
+import ConfirmationPayment from "./core/confirmation";
+import UpdateProduct from "./admin/updateproduct";
+
 
 const Routes = () => {
- return(
-  <Router>
-   <React.Fragment>
-   <NavigationBar />
-     <Switch>
-       <Route exact path="/" component={Home}/>
-       <Route path="/cart" exact component={Cart}/>
-       <Route path="/checkout" exact component={CheckOut}/>
-       <Route path="/signin" component={SignIn}/>
-       <Route path="/signup" component={SignUp}/>
-       <Route path="/shop" component={Shop}/>
-       <Route path="/reviewcart" exact component={Review}/>
-       <Route path="/product/:productId" exact component={Product} />
-       <PrivateRoute
-          path="/paymentconfirmation"
-          component={PaymentConfirmation}/>
-       <PrivateRoute
-          path="/user/dashboard"
-          component={Dashboard}/>
-       <PrivateRoute
-          path="/profile/:userId"
-          component={Profile}/>
-       <PrivateRoute
-          path="/user/purchasehistory"
-          component={PurchaseHistory}/>
-       <AdminRoute
-          path="/admin/dashboard"
-          component={AdminDashboard}/>
-       <AdminRoute
-          path="/create/category"
-          component={AddCategory}/>
-       <AdminRoute
-          path="/create/product"
-          component={AddProduct}/>
-       <AdminRoute
-          path="/admin/orders"
-          component={Orders}/>
-       <AdminRoute
-          path="/admin/products"
-          component={ManageProducts}/>
-       <AdminRoute
-          path="/admin/payments"
-          component={ManagePayments}/>
-       <AdminRoute
-          path="/admin/product/update/:productId"
-          component={UpdateProduct}/>
-     </Switch>
-   </React.Fragment>
-  </Router>
+  return(
+    <BrowserRouter>
+     <PrimarySearchAppBar/>
+       <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/shop" exact component={Shop}/>
+        <Route path="/signin" exact component={SignIn}/>
+        <Route path="/signup" exact component={SignUp}/>
+        <Route path="/cart" exact component={Cart}/>
+        <Route path="/reviewcart" exact component={Review}/>
+        <Route path="/caraorder" exact component={Order}/>
+        <Route path="/review-products" exact component={ReviewProducts}/>
+        <Route path="/konfirmasi-pembayaran" exact component={ConfirmationPayment}/>
+        <Route path="/Resi-Pengiriman" exact component={ResiShipment}/>
+        <Route path="/Hubungi-Kami" exact component={ContactUs}/>
+        <Route path="/product/:productId" exact component={Product} />
+        <PrivateRoute
+               path="/paymentconfirmation"
+               exact
+               component={PaymentConfirmation}/>
+        <PrivateRoute
+               path="/user/dashboard"
+               exact
+               component={Dashboard}/>
+        <PrivateRoute
+               path="/profile/:userId"
+               exact
+               component={Profile}/>
+        <PrivateRoute
+               path="/user/purchasehistory"
+               exact
+               component={PurchaseHistory}/>
+        <AdminRoute
+               path="/admin/dashboard"
+               exact
+               component={AdminDashboard}/>
+        <AdminRoute
+               path="/create/category"
+               exact
+               component={AddCategory}/>
+        <AdminRoute
+               path="/create/product"
+               exact
+               component={AddProduct}/>
+        <AdminRoute
+               path="/admin/orders"
+               exact
+               component={Orders}/>
+        <AdminRoute
+               path="/admin/products"
+               exact
+               component={ManageProducts}/>
+        <AdminRoute
+               path="/admin/payments"
+               exact
+               component={ManagePayments}/>
+        <AdminRoute
+               path="/admin/product/update/:productId"
+               exact
+               component={UpdateProduct}/>
+       </Switch>
+   </BrowserRouter>
  );
 };
 
